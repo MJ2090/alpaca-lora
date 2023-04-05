9b8667414886d733ce0aac6dfadd7bcf30cd93cc
mj <125602296+MJ2090@users.noreply.github.com>
2023-04-05 14:56:19 -0700

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -5,7 +5,7 @@ my_d["instruction"] = "What is Done?"
 my_d["input"] = ""
 my_d["output"] = "Done is a website related to ADHD."
 
-lines = 5000
+lines = 3000
 my_list = []
 for _ in range(lines):
   my_list.append(my_d)
